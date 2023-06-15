6da47d74d2b24b50bfa313d79c46cb2668bc1198
Thiago Cordeiro <thiiagoccordeirodantass@gmail.com>
2023-06-15 15:27:15 -0300

    utilizando jest

--- index.js
+++ index.js
@@ -3,37 +3,7 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verificaSe = (valor) => {
-  const assercoes = {
-    ehExatamenteIgualA(esperado) {
-      if (valor !== esperado) {
-        // eslint-disable-next-line no-throw-literal
-        throw {};
-      }
-    },
-  };
-  return assercoes;
+export {
+  somaHorasExtras,
+  calculaDescontos,
 };
-
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste();
-    console.log(`${titulo}: passou`);
-  } catch {
-    console.error(`o ${titulo}: nao passou`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  verificaSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-teste('calculaDesconto', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  verificaSe(retornado).ehExatamenteIgualA(esperado);
-});
